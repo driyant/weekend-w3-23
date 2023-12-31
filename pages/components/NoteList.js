f48fce7d8426fb6ee292f3b0effd7747a3b771eb
driyant <message.driyant@outlook.com>
2023-12-31 16:38:09 +0700

    feat: separate components

---
+++ pages/components/NoteList.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+import {
+  Box,
+  Heading,
+  TableContainer,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+} from "@chakra-ui/react";
+import NoteListItem from "./NoteListItem";
+
+const NoteList = () => {
+  return (
+    <>
+      <Box
+        width="80%"
+        marginBottom="2rem"
+        boxShadow="0px 8px 24px rgba(149, 157, 165, 0.2)"
+        backgroundColor="#FFF"
+        borderRadius="6px"
+        padding="1rem"
+      >
+        <Heading as="h5" size="sm">
+          Note list
+        </Heading>
+        <TableContainer marginTop="1rem">
+          <Table variant="striped" colorScheme="teal">
+            <Thead>
+              <Tr>
+                <Th>list</Th>
+                <Th>operation</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <NoteListItem />
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </>
+  );
+};
+
+export default NoteList;
